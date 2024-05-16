11ae911f6951dcb4c185f83b942060aa0acfabd3
conbanwa <35380552+conbanwa@users.noreply.github.com>
2024-05-16 21:29:34 +0800

    Update BinanceFutures_test.go

--- ex/binance/BinanceFutures_test.go
+++ ex/binance/BinanceFutures_test.go
@@ -5,52 +5,69 @@ import (
 	"github.com/conbanwa/wstrader"
 	"github.com/conbanwa/wstrader/cons"
 	"testing"
-
-	"github.com/conbanwa/logs"
 )
+
+const (
+	testnetApiKey       = "YOUR_KEY"
+	testnetApiKeySecret = "YOUR_KEY_SECRET"
+)
+
+func skipKey(t *testing.T) {
+	if testnetApiKey == "YOUR_KEY" {
+		t.Skip("Skipping testing without testnetApiKey")
+	}
+}
 
 var baDapi = NewBinanceFutures(&wstrader.APIConfig{
 	HttpClient:   http.DefaultClient,
-	ApiKey:       "",
-	ApiSecretKey: "",
+	ApiKey:       testnetApiKey,
+	ApiSecretKey: testnetApiKeySecret,
 })
 
-func init() {
-	logs.Log.Level = logs.L_DEBUG
-}
 func TestBinanceFutures_GetFutureDepth(t *testing.T) {
+	skipKey(t)
 	t.Log(baDapi.GetFutureDepth(cons.ETH_USD, cons.QUARTER_CONTRACT, 10))
 }
 func TestBinanceSwap_GetFutureTicker(t *testing.T) {
+	skipKey(t)
 	ticker, err := baDapi.GetFutureTicker(cons.LTC_USD, cons.SWAP_CONTRACT)
 	t.Log(err)
 	t.Logf("%+v", ticker)
 }
 func TestBinance_GetExchangeInfo(t *testing.T) {
+	skipKey(t)
 	baDapi.GetExchangeInfo()
 }
 func TestBinanceFutures_GetFutureUserinfo(t *testing.T) {
+	skipKey(t)
 	t.Log(baDapi.GetFutureUserinfo())
 }
 func TestBinanceFutures_PlaceFutureOrder(t *testing.T) {
+	skipKey(t)
 	//1044675677
 	t.Log(baDapi.PlaceFutureOrder(cons.BTC_USD, cons.QUARTER_CONTRACT, "19990", "2", cons.OPEN_SELL, 0, 10))
 }
 func TestBinanceFutures_LimitFuturesOrder(t *testing.T) {
+	skipKey(t)
 	t.Log(baDapi.LimitFuturesOrder(cons.BTC_USD, cons.QUARTER_CONTRACT, "20001", "2", cons.OPEN_SELL))
 }
 func TestBinanceFutures_MarketFuturesOrder(t *testing.T) {
+	skipKey(t)
 	t.Log(baDapi.MarketFuturesOrder(cons.BTC_USD, cons.QUARTER_CONTRACT, "2", cons.OPEN_SELL))
 }
 func TestBinanceFutures_GetFutureOrder(t *testing.T) {
+	skipKey(t)
 	t.Log(baDapi.GetFutureOrder("1045208666", cons.BTC_USD, cons.QUARTER_CONTRACT))
 }
 func TestBinanceFutures_FutureCancelOrder(t *testing.T) {
+	skipKey(t)
 	t.Log(baDapi.FutureCancelOrder(cons.BTC_USD, cons.QUARTER_CONTRACT, "1045328328"))
 }
 func TestBinanceFutures_GetFuturePosition(t *testing.T) {
+	skipKey(t)
 	t.Log(baDapi.GetFuturePosition(cons.BTC_USD, cons.QUARTER_CONTRACT))
 }
 func TestBinanceFutures_GetUnfinishFutureOrders(t *testing.T) {
+	skipKey(t)
 	t.Log(baDapi.GetUnfinishFutureOrders(cons.BTC_USD, cons.QUARTER_CONTRACT))
 }
