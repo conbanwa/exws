6056f45fc89aa70c8b4e13036eeaa11df9ffde02
conbanwa <35380552+conbanwa@users.noreply.github.com>
2024-05-16 21:03:31 +0800

    Update BinanceFutures_test.go
    Update binance_futures_trade_test.go
    
    Update okx_test.go
    
    Update BinanceFutures_test.go
    
    Update BinanceSwap_test.go

--- ex/okx/okx_test.go
+++ ex/okx/okx_test.go
@@ -8,6 +8,17 @@ import (
 	"testing"
 )
 
+const (
+	testnetApiKey       = "YOUR_KEY"
+	testnetApiKeySecret = "YOUR_KEY_SECRET"
+)
+
+func skipKey(t *testing.T) {
+	if testnetApiKey == "YOUR_KEY" {
+		t.Skip("Skipping testing without testnetApiKey")
+	}
+}
+
 func newOKExV5Client() *OKX {
 	return NewOKExV5(&wstrader.APIConfig{
 		//HttpClient: &http.Client{
@@ -20,20 +31,23 @@ func newOKExV5Client() *OKX {
 		//	},
 		//},
 		Endpoint:      "https://www.okx.com",
-		ApiKey:        "",
-		ApiSecretKey:  "",
+		ApiKey:        testnetApiKey,
+		ApiSecretKey:  testnetApiKeySecret,
 		ApiPassphrase: "",
 	})
 }
 func TestOKExV5_GetTicker(t *testing.T) {
+	skipKey(t)
 	o := newOKExV5Client()
 	fmt.Println(o.GetTickerV5("BTC-USD-SWAP"))
 }
 func TestOKExV5_GetDepth(t *testing.T) {
+	skipKey(t)
 	o := newOKExV5Client()
 	fmt.Println(o.GetDepthV5("BTC-USD-SWAP", 0))
 }
 func TestOKExV5_GetKlineRecordsV5(t *testing.T) {
+	skipKey(t)
 	o := newOKExV5Client()
 	fmt.Println(o.GetKlineRecordsV5("BTC-USD-SWAP", cons.KLINE_PERIOD_1H, &url.Values{}))
 }
